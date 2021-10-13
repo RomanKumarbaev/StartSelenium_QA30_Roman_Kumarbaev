d6a74c3d001cd13ca5c57f77f4b3eb60955c96b3
Roman Kumarbaev <91917230+RomanKumarbaev@users.noreply.github.com>
2021-10-13 20:47:44 +0300

    TableTest

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -1,8 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartSelenium {
     WebDriver wd;
@@ -17,6 +21,43 @@ public class StartSelenium {
     }
 @Test
     public void testName(){
+    WebElement element = wd.findElement(By.tagName("a"));
+    wd.findElement(By.cssSelector("a"));
+    wd.findElement(By.cssSelector("div"));
+
+    List<WebElement> elements = wd.findElements(By.tagName("a"));
+    System.out.println(elements.size());
+
+    wd.findElement(By.className("container"));
+
+    wd.findElement(By.cssSelector(".container"));
+
+    wd.findElement(By.className("navbar-component_nav__1X_4m"));
+
+    wd.findElement(By.id("root"));
+    wd.findElement(By.cssSelector("#root"));
+
+  /*  wd.findElement(By.className(""));
+
+
+    wd.findElement(By.id(""));
+
+    wd.findElement(By.tagName(""));
+*/
+
+    wd.findElement(By.linkText("HOME"));
+
+    wd.findElement(By.cssSelector("[href]"));
+
+    wd.findElement(By.cssSelector("[href='/login']"));
+
+    wd.findElement(By.cssSelector("[href ^='/lo']")); //start
+
+    wd.findElement(By.cssSelector("[href $='in']")); //end
+    wd.findElement(By.cssSelector("[href *='ogi']")); //contains
+
+
+    wd.findElement(By.partialLinkText("lo"));
 
 
 }
